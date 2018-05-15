1bffdefc45f67561e3a0561d80ae0efc8326fe14
Yasu <yassantune@gmail.com>
2018-05-15 16:16:38 +0000

    first commit from day1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
+//console.log(MessageItem);
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      //<h1>Hello React :)</h1>
+      {MessageList},
+      {ChatBar}
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,13 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader"
+            }]
       }
     ]
   }
