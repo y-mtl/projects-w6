0cf8fecd02a6f7bf9d171097b564b0797cfc7984
Yasu <yassantune@gmail.com>
2018-05-17 02:57:54 +0000

    socket was added, component has been changed for it

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,77 @@ import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
+// WebSocket WebSocket(
+//   in DOMString "ws://localhost:3001",
+//   in optional DOMString protocols
+// );
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       currentUser: {name: "Bob"},
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: "1"
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: "2"
-        }
-      ],
-
+      messages: []
     };
-    this.getMsg = this.getMsg.bind(this)
+    this.getMsg = this.getMsg.bind(this);
+    //this.socket = this.socket.bind(this);console.log(this.socket);
+  }
+
+  socketInit(){
+    const connection = new WebSocket("ws://localhost:3001");
+    return connection;
   }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket = this.socketInit();
+
+    this.socket.onopen = event => {
+      console.log('Connected to server');
+    }
+
+    let receivedMsg = {};
+    //console.log(1, this);
+    this.socket.onmessage = function(event) {
+      let msg = [];
+      if(this.state.messages){
+        const receivedMsg = JSON.parse(event.data);
+        const messages = this.state.messages.concat(receivedMsg);
+
+        this.setState({
+          messages: messages
+        })
+      }
+    }.bind(this)
+    // the above this (from this.socket) and this.setState(in the onmessage block)
+    // is not the same. need to use 'bind(this)' to be the same 'this'.
   }
 
   getMsg(msg){
-    const msgId = this.state.messages.length + 1;console.log(msgId);
-    const messages = this.state.messages.concat(msg);
-    this.setState({
-      currentUser: {name: msg.username},
-      messages: messages,
-      id: msgId
-      })
+    //const msgId = this.state.messages.length + 1;
+    //const messages = this.state.messages.concat(msg);
+    this.socket.send(JSON.stringify(msg));
+    // this.setState = {
+    //   currentUser: {name: "Bob"},
+    //   messages: [
+    //     {
+    //       username:,
+    //       content:,
+    //       id:
+    //     }]
+    // };
+    //console.log("msg", msg);
+    // const messages = this.state.connection.onmessage = (event) => {
+    //   var receivedMsg = JSON.parse(event.data);
+    //   console.log(receivedMsg);
+    // }
+    // const messages = this.state.messages.concat(msg);
+    // this.setState({
+    //   currentUser: {name: msg.username},
+    //   messages: this.state.messages,
+    //   id: this.state.messages.id
+    // })
+    //console.log('After this state:', this.state.messages);
   }
   render() {
     return (
